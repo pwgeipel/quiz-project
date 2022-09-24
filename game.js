1ad47656dbe81b5869cac633a20baa75bb0c87cb
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-24 16:16:29 -0500

    nextQuestion

--- game.js
+++ game.js
@@ -1,13 +1,48 @@
-var answer = Array.from(document.getElementsByClassName("answer-text"))
-console.log(answer);
+var question = document.getElementById('question');
+var answers = Array.from(document.getElementsByClassName("answer-text"));
+
+const wrongTime = 10;
+let = acceptAnswers = true;
+let = score = 0;
+let = availableQuestions = [];
+let = currentQuestion = {};
 
 let questions = [
     {
-        question: "Which of the following was NOT directed by Charlie Chaplin?"
-        answer1: "The Gold Rush"
-        answer2: "Modern Times"
-        answer3: "The General"
-        answer4: "City Lights"
+        question: "Which of the following was NOT directed by Charlie Chaplin?",
+        answer1: "The Gold Rush",
+        answer2: "Modern Times",
+        answer3: "The General",
+        answer4: "City Lights",
         correctAnswer: 3
     },
+    {
+        question: "In what year was Apple founded?",
+        answer1: "1975",
+        answer2: "1976",
+        answer3: "1977",
+        answer4: "1978",
+        correctAnswer: 2
+    }
 ]
+
+beginGame = () => {
+    score = 0;
+    questionCounter = 0;
+    availableQuestions = [...questions]
+    nextQuestion ();
+}
+
+nextQuestion = () => {
+    questionCounter++;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    answers.forEach(answer => {
+        var number = answer.dataset["number"];
+        answer.innerText = currentQuestion["answer" + number];
+    })
+}
+
+beginGame ();
