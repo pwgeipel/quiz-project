245feece8a3ba2ca6b95cfb1b537844a290c224f
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-24 14:27:33 -0500

    question, data-number

---
+++ game.js
@@ -0,0 +1,13 @@
+var answer = Array.from(document.getElementsByClassName("answer-text"))
+console.log(answer);
+
+let questions = [
+    {
+        question: "Which of the following was NOT directed by Charlie Chaplin?"
+        answer1: "The Gold Rush"
+        answer2: "Modern Times"
+        answer3: "The General"
+        answer4: "City Lights"
+        correctAnswer: 3
+    },
+]
