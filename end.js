d350d0d3727c2ca9f974728a979a13573531ca27
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-26 23:05:44 -0500

    read.me

--- end.js
+++ end.js
@@ -7,23 +7,22 @@ let scores;
 
 scoreEl.innerText = score;
 
-
+//Retrieves High scores from local storage
 if (localStorage.getItem('High Scores')) {
     scores = [localStorage.getItem('High Scores')];
 } else {
     scores = [];
 }
+//Saves high score and initials to local storage
 function saveInitials() {
     scores.push('Initials: ${username.value) || Score: $(score)');
     localStorage.setItem('High Scores', scores);
 }
-
-
-
+//Allows submission after something has been entered into input field
 username.addEventListener('keyup', () => {
     saveScoreButton.disabled = !username.value;
 })
-
+//Prevents page from reloading
 saveHighScore = click => {
     click.preventDefault();
     saveInitials();
--- highScores.js
+++ highScores.js
@@ -2,7 +2,7 @@ const highScoresList = document.getElementById('highScoresList');
 const localScores = localStorage.getItem('High Scores').split(',')
 
 console.log(localScores);
-
+//creates list of high scores
 localScores.forEach(score => {
     var li = document.createElement('li');
     li.textContent = score;
